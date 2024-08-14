c27381af300b3482224e78ab9739da1906080873
GuiRosario <gdasilvarosario@gmail.com>
2024-08-14 17:16:30 -0300

    Adicionando o service

--- src/main/java/com/example/catalogo/services/FilmeService.java
+++ src/main/java/com/example/catalogo/services/FilmeService.java
@@ -1,7 +1,11 @@
 package com.example.catalogo.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.*;
 import com.example.catalogo.entities.Filme;
 import com.example.catalogo.repositories.CatalogoRepository;
 
@@ -12,5 +16,38 @@ public class FilmeService {
     public Filme salvar(Filme filme){
         filme = repository.save(filme);
         return filme;
+    }
+
+    public Filme alterar(Filme filme){
+        if(Objects.nonNull(filme.getId())){
+            filme = repository.save(filme);
+        }else{
+            System.out.println("Filme nao encontrado!");
+        }
+        return filme;
+    }
+
+    public List<Filme> listar(){
+        return repository.findAll();
+    }
+
+    public Boolean excluir(Integer id){
+        try{
+            repository.deleteById(id);
+        } catch (Exception e){
+            System.out.printf("Erro ao realizar Exclusao ",e);
+            return false;
+        }
+        return true;
+    }
+
+    public Optional<Filme> consultarPorId(Integer id){
+        try{
+            Optional<Filme> filme = repository.findById(id);
+            return filme;
+        } catch(Exception e){
+            System.out.printf("Erro ao retornar o filme ", e);
+        }
+        return null;
     }
 }
